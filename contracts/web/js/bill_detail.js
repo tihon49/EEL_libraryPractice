8ce09ce645bb04f611d5707dbc2990beb3a8605b
tihon49 <tihon49@gmail.com>
2021-01-15 18:14:04 +0300

    bill_repair autofill logic done

--- contracts/web/js/bill_detail.js
+++ contracts/web/js/bill_detail.js
@@ -7,40 +7,12 @@ async function bill_detail() {
 bill_detail();
 
 
-// let table = document.querySelector('#bills_table'),
-//     repeir_table = document.querySelector('#bills_repair_table');
-
 let repeir_table = document.querySelector('#bills_repair_table');
 
 
 // функция отрисовки DOM
 eel.expose(print_bill_detail_data);
 function print_bill_detail_data(data){
-    // отрисовываем актуальные данные текущего счета
-    // let tr = document.createElement('tr'),  //создаем строку в таблице (тег <tr>)
-    //     td_bill_number = document.createElement('td'),
-    //     td_bill_sum = document.createElement('td'),
-    //     td_bill_date = document.createElement('td'),
-    //     td_act_number = document.createElement('td'),
-    //     td_act_sum = document.createElement('td'),
-    //     td_act_date = document.createElement('td');
-
-    // td_bill_number.innerHTML = data.bill_number;
-    // td_bill_date.innerHTML = data.bill_date;
-    // td_bill_sum.innerHTML = data.bill_sum;
-    // td_act_number.innerHTML = data.act_number;
-    // td_act_date.innerHTML = data.act_date;
-    // td_act_sum.innerHTML = data.act_sum;
-
-    // tr.append(td_bill_number);
-    // tr.append(td_bill_date);
-    // tr.append(td_bill_sum);
-    // tr.append(td_act_number);
-    // tr.append(td_act_date);
-    // tr.append(td_act_sum);
-
-    // table.append(tr);
-
     // форма изменения текущего счета
     let tr_repair = document.createElement('tr'),
         td_form_bill_number = document.createElement('td'),
@@ -54,37 +26,37 @@ function print_bill_detail_data(data){
 
     // формы
     let form_bill_number = document.createElement('form'); 
-    form_bill_number.innerHTML = '<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_number" required placeholder="Номер счета">';
+    form_bill_number.innerHTML = `<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_number" required value="${data.bill_number}">`;
     td_form_bill_number.append(form_bill_number);
     tr_repair.append(td_form_bill_number);
 
     let form_bill_date = document.createElement('form');
-    form_bill_date.innerHTML = '<input style="background-color: rgb(248, 248, 231); width: 70px; text-align: center;" type="text" id="bill_date" required placeholder="Дата счета" value="11.12.1985">';
+    form_bill_date.innerHTML = `<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="text" id="bill_date" required value="${data.bill_date}">`;
     td_form_bill_date.append(form_bill_date);
     tr_repair.append(td_form_bill_date);
     
 
     let form_bill_sum = document.createElement('form');
-    form_bill_sum.innerHTML = '<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_sum" required placeholder="Сумма счета" value="24000">';
+    form_bill_sum.innerHTML = `<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_sum" required value="${data.bill_sum}">`;
     td_form_bill_sum.append(form_bill_sum);
     tr_repair.append(td_form_bill_sum);
 
     let form_act_number = document.createElement('form');
-    form_act_number.innerHTML = '<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_sum" required placeholder="Номер акта" value="1">';
+    form_act_number.innerHTML = `<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_sum" required value="${data.act_number}">`;
     td_form_act_number.append(form_act_number);
     tr_repair.append(td_form_act_number);
 
     let form_act_date = document.createElement('form');
-    form_act_date.innerHTML = '<input style="background-color: rgb(248, 248, 231); width: 70px; text-align: center;" type="text" id="bill_date" required placeholder="Дата акта" value="11.12.1985">';
+    form_act_date.innerHTML = `<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="text" id="bill_date" required value="${data.act_date}">`;
     td_form_act_date.append(form_act_date);
     tr_repair.append(td_form_act_date);
 
     let form_act_sum = document.createElement('form');
-    form_act_sum.innerHTML = '<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_date" required placeholder="Сумма акта" value="24000">';
+    form_act_sum.innerHTML = `<input style="background-color: rgb(248, 248, 231); width: 100px; text-align: center;" type="number" id="bill_date" required value="${data.act_sum}">`;
     td_form_act_sum.append(form_act_sum);
     tr_repair.append(td_form_act_sum);
 }
 
 
-//TODO: 1. Автозаполнение формы актуальными данными
-//      2. При нажатии на кнопку "сохранить" сохранять изменеия в счете
+
+//TODO: При нажатии на кнопку "сохранить" сохранять изменеия в счете
